d692ec3301f2ade4a2df5629ce2fbef41a94b94b
Darren584 <darren.alweyi@strathmore.edu>
2025-07-14 08:21:00 +0300

    Delete Budget Tracker React directory

--- Budget Tracker React/Notifications.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function Notifications({ messages }) {
-  return <div id='notification-container'>
-    {messages.map((msg, i) => (
-      <div key={i} className='notification'>{msg}</div>
-    ))}
-  </div>;
-}
--- Budget Tracker React/src/hooks/useLocalStorage.js
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from 'react';
-export default function useLocalStorage(key, initial) {
-  const [value, setValue] = useState(() => {
-    const stored=localStorage.getItem(key);
-    return stored?JSON.parse(stored):initial;
-  });
-  useEffect(()=>localStorage.setItem(key,JSON.stringify(value)),[key,value]);
-  return [value,setValue];
-}
